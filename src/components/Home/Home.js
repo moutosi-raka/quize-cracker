969cf20046caea6a60a75c43414107dec2b3ec71
moutosi-raka <raka29077997@gmail.com>
2022-10-11 15:27:26 +0600

    added quizCart and load API data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import QuizCart from '../QuizCart/QuizCart';
+
 import Slider from '../Slider/Slider';
 
 const Home = () => {
+    const courseCarts = useLoaderData().data;
     return (
         <div>
             <Slider></Slider>
+            {
+            courseCarts.map(cart => <QuizCart 
+                cart={cart}
+                key={cart.id}
+                ></QuizCart>)
+                
+            }
+            
         </div>
     );
 };
---
+++ src/components/QuizCart/QuizCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuizCart = ({cart}) => {
+    console.log(cart)
+    return (
+        <div>
+            <h1>quiz</h1>
+        </div>
+    );
+};
+
+export default QuizCart;
