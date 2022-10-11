8caec7fc9aa373789d076012184bcff90a1d3b11
moutosi-raka <raka29077997@gmail.com>
2022-10-11 23:12:46 +0600

    added eye icons

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Option from '../Option/Option';
+
+
 import './Question.css'
 
 const Question = ({questionDetails,inx}) => {
@@ -7,7 +9,10 @@ const Question = ({questionDetails,inx}) => {
   
     return (
         <div className='question-container'>
+            <div className='question-header'>
             <h4>{inx+1}. {question}</h4>
+            <i class="fa-solid fa-eye"></i>
+            </div>
             <div className='options-container'>
                 {
                     options.map((option, index) => <Option key={index} option={option}></Option>)
