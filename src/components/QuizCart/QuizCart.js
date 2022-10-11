a65d5e83c2edf32cf90c7d679ebf0a0081666522
moutosi-raka <raka29077997@gmail.com>
2022-10-11 19:24:49 +0600

    quizCart design

--- src/components/QuizCart/QuizCart.js
+++ src/components/QuizCart/QuizCart.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './QuizCart.css'
 
 const QuizCart = ({cart}) => {
     console.log(cart)
+    const {name, id, logo,total} = cart;
     return (
-        <div>
-            <h1>quiz</h1>
+        <div className='cart-container'>
+            <div className='cart-logo'>
+                <img src={logo} alt="" />
+            </div>
+            <div className='cart-details'>
+                <div>
+                <h3>{name}</h3>
+            <p> Total Quiz: {total}</p>
+            <Link><button className='practice-btn'>Start Practice</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
