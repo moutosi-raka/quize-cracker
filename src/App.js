a39ca6eaa63d9768281e9268335f336abb1f751b
moutosi-raka <raka29077997@gmail.com>
2022-10-12 12:46:27 +0600

    create rechart and design

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         },
         {
           path: '/statistic',
+          loader: async()=> fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistic></Statistic>
         },
         {
--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import './Statistic.css';
 
 const Statistic = () => {
-    return (
-        <div>
-            <h1>this is statistic</h1>
-        </div>
-    );
+  const data = useLoaderData().data;
+
+  return (
+    <div className="chart">
+      <LineChart width={500} height={400} data={data}>
+        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+    </div>
+  );
 };
 
 export default Statistic;
