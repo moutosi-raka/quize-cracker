632f4421dedca92d266628502859c533ec84e8ac
moutosi-raka <raka29077997@gmail.com>
2022-10-12 12:20:07 +0600

    create responsive navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import {NavLink } from 'react-router-dom';
-import './Header.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div>
-            <NavLink className='logo' to='/'>Knowledge World</NavLink>
-            </div>
-            <div>
-            <NavLink  className={({ isActive }) =>
-              isActive ? 'activeStyle' : 'inactive'
-            } to="/">Home</NavLink>
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="header">
+      <div  className='nav-icons'>
+        {open ? <i onClick={() => setOpen(!open)}  className="fa-solid fa-xmark"></i> 
+        : <i onClick={() => setOpen(!open)} className="fa-solid fa-bars"></i>
+        }
+      </div>
+      <div>
+        <NavLink className="logo" to="/">
+          Knowledge World
+        </NavLink>
+      </div>
+      <div className={`nav-link ${open ? 'top-4' : 'top-120'}`}>
+        <NavLink
+          className={({ isActive }) => (isActive ? "activeStyle" : "inactive")}
+          to="/"
+        >
+          Home
+        </NavLink>
 
-            <NavLink className={({ isActive }) =>
-              isActive ? 'activeStyle' : 'inactive'
-            } to="/statistic">Statistic</NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "activeStyle" : "inactive")}
+          to="/statistic"
+        >
+          Statistic
+        </NavLink>
 
-            <NavLink className={({ isActive }) =>
-              isActive ? 'activeStyle' : 'inactive'
-            } to="/blog">Blog</NavLink>
-            </div>
-            
-        </div>
-    );
+        <NavLink
+          className={({ isActive }) => (isActive ? "activeStyle" : "inactive")}
+          to="/blog"
+        >
+          Blog
+        </NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
